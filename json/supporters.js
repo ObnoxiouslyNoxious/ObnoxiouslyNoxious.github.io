3f93d8e455c86b0c3c34441f3cc6660c321799af
ObnoxiouslyNoxious <285436119+ObnoxiouslyNoxious@users.noreply.github.com>
2026-09-05 23:24:25 +1000

    Update Ko-Fi Supporters

---
+++ json/supporters.js
@@ -0,0 +1,23 @@
+window.SUPPORTERS_DATA = {
+  "subscribers": {
+    "gold": [],
+    "silver": [],
+    "bronze": [
+      "Itsbringr"
+    ]
+  },
+  "recent": [
+    {
+      "name": "Beornvig",
+      "amount": "$10.00"
+    },
+    {
+      "name": "PotatoHead",
+      "amount": "$2.00"
+    },
+    {
+      "name": "iDestin",
+      "amount": "$50.00"
+    }
+  ]
+};
